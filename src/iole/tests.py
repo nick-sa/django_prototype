feccb2132734b7742ec286a7dca63554a77b9a34
NS <ns@eduroam-141-23-192-74.wlan.tu-berlin.de>
2025-07-23 10:28:14 +0200

    first tests

--- src/iole/tests.py
+++ src/iole/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from src.iole.models import InpFile, Node
 
 # Create your tests here.
 def test_test():
@@ -6,4 +7,23 @@ def test_test():
 
 class firstTest(TestCase):
     def test_something(self):
-        self.assertTrue(1+1==1)
+        self.assertTrue(1+1==2)
+
+
+class InpFileTestCase(TestCase):
+    def setUp(self):
+        InpFile.objects.create(filename = "testFileName")
+        #InpFile.objects.create(fileName="testFileName2")
+    def test_inpFile_created_successfully(self):
+        ourInpFile = InpFile.objects.filter(filename = "testFileName")
+        #self.assertEqual(ourInpFile.first().fileName, "testFileName")
+
+
+class NodeTestCase(TestCase):
+    def setUp(self):
+        InpFile.objects.create(filename = "testFileName")
+        Node.objects.create( name = "nodeName", x_coordinate = 2.0, y_coordinate = 4.0, has_sensor = True, inp_file = InpFile.objects.get(filename = "testFileName"))
+
+    def test_node(self):
+        Node.objects.filter( name = "nodeName")
+
